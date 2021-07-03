038240412171645b03e8f1ca94565edaa9bf267d
nforay <nforay@student.42.fr>
2021-07-03 17:18:34 +0200

    Added Map Member Functions

--- incs/map.hpp
+++ incs/map.hpp
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:21:43 by nforay            #+#    #+#             */
-/*   Updated: 2021/07/02 19:47:48 by nforay           ###   ########.fr       */
+/*   Updated: 2021/07/03 17:17:13 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,338 @@ namespace ft
 
 		public:
 
-			
+			/**
+			 * 
+			*/
+			explicit map(const key_compare& comp = key_compare(),
+				const allocator_type& alloc = allocator_type())
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			template <class InputIterator>
+			map(InputIterator first, InputIterator last,
+				const key_compare& comp = key_compare(),
+				const allocator_type& alloc = allocator_type())
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			map(const map& x)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			~map()
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			map& operator=(const map& x)
+			{
+				
+			}
+
+/*
+** --------------------------------- ITERATORS ---------------------------------
+*/
+
+			/**
+			 * 
+			*/
+			iterator begin()
+			{
+
+			}
+
+			/**
+			 * 
+			*/
+			const_iterator begin() const
+			{
+
+			}
+
+			/**
+			 * 
+			*/
+			iterator end()
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			const_iterator end() const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			reverse_iterator rbegin()
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			const_reverse_iterator rbegin() const
+			{
+
+			}
+
+			/**
+			 * 
+			*/
+			reverse_iterator rend()
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			const_reverse_iterator rend() const
+			{
+
+			}
+
+/*
+** --------------------------------- CAPACITY ----------------------------------
+*/
+
+			/**
+			 * 
+			*/
+			bool empty() const
+			{
+
+			}
+
+			/**
+			 * 
+			*/
+			size_type size() const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			size_type max_size() const
+			{
+				
+			}
+
+/*
+** ------------------------------ ELEMENT ACCESS -------------------------------
+*/
+
+			/**
+			 * 
+			*/
+			mapped_type& operator[](const key_type& k)
+			{
+				
+			}
+
+/*
+** -------------------------------- MODIFIERS ----------------------------------
+*/
+
+			/**
+			 * 
+			*/
+			pair<iterator,bool> insert(const value_type& val)
+			{
+
+			}
+
+			/**
+			 * 
+			*/
+			iterator insert(iterator position, const value_type& val)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			template <class InputIterator>
+			void insert(InputIterator first, InputIterator last)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			void erase(iterator position)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			size_type erase(const key_type& k)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			void erase(iterator first, iterator last)
+			{
+
+			}
+
+			/**
+			 * 
+			*/
+			void swap(map& x)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			void clear()
+			{
+				
+			}
+
+/*
+** -------------------------------- OBSERVERS ----------------------------------
+*/
+
+			/**
+			 * 
+			*/
+			key_compare key_comp() const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			value_compare value_comp() const
+			{
+				
+			}
+
+/*
+** -------------------------------- OPERATIONS ---------------------------------
+*/
+
+			/**
+			 * 
+			*/
+			iterator find(const key_type& k)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			const_iterator find(const key_type& k) const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			size_type count(const key_type& k) const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			iterator lower_bound(const key_type& k)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			const_iterator lower_bound(const key_type& k) const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			iterator upper_bound(const key_type& k)
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			const_iterator upper_bound(const key_type& k) const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			pair<const_iterator,const_iterator> equal_range(const key_type& k) const
+			{
+				
+			}
+
+			/**
+			 * 
+			*/
+			pair<iterator,iterator> equal_range(const key_type& k)
+			{
+				
+			}
+
+/*
+** -------------------------------- ALLOCATOR ----------------------------------
+*/
+
+			/**
+			 * 
+			*/
+			allocator_type get_allocator() const
+			{
+				
+			}
+
+/*
+** ---------------------------- PRIVATE FUNCTIONS ------------------------------
+*/
 
 		private:
 
--- incs/utils.hpp
+++ incs/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 03:33:28 by nforay            #+#    #+#             */
-/*   Updated: 2021/06/25 19:51:06 by nforay           ###   ########.fr       */
+/*   Updated: 2021/07/01 17:16:49 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,81 @@ namespace ft
 			++first2;
 		}
 		return (first2!=last2);
+	}
+
+	template<class T1, class T2>
+	struct pair
+	{
+		public:
+			typedef T1		first_type;
+			typedef T2		second_type;
+
+			first_type		first;
+			second_type		second;
+
+			pair() : first(first_type()), second(second_type()) {}
+			pair(const first_type& x, const second_type& y)
+			: first(x), second(y) {}
+			template<class U, class V>
+			pair(const pair<U, V>& src) : first(src.first), second(src.second) {}
+			~pair() {}
+
+			pair& operator=(const pair& other)
+			{
+				first = other.first;
+				second = other.second;
+				return *this;
+			}
+	};
+
+	template<class T1, class T2>
+	bool operator==(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return lhs.first == rhs.first && lhs.second == rhs.second;
+	}
+
+	template< class T1, class T2 >
+	bool operator!=(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	template< class T1, class T2 >
+	bool operator<(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		if (lhs.first < rhs.first)
+			return true;
+		if (lhs.second < rhs.second)
+			return true;
+		return false;
+	}
+
+	template< class T1, class T2 >
+	bool operator<=(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		if (lhs.first <= rhs.first)
+			return true;
+		if (lhs.second <= rhs.second)
+			return true;
+		return false;
+	}
+
+	template< class T1, class T2 >
+	bool operator>(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return !(lhs <= rhs);
+	}
+
+	template< class T1, class T2 >
+	bool operator>=(const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs)
+	{
+		return !(lhs < rhs);
+	}
+
+	template<class T1, class T2>
+	ft::pair<T1,T2> make_pair(T1 t, T2 u)
+	{
+		return ft::pair<T1, T2>(t, u);
 	}
 }
 
